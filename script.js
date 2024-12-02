71d2de788048c88dd87e1bee7e1a5406845390e1
Dubons <rosaliodubon@gmail.com>
2024-12-01 19:16:19 -0600

    se realizan cambios, en la seccion de habilidades

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function seleccionar(){
     menuVisible = false;
 }
 //Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
+/*function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
@@ -38,7 +38,7 @@ function efectoHabilidades(){
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
     efectoHabilidades();
-} 
+} */
 
 //metodo para descargar
 function descargarPDF() {
