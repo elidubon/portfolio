0e54e9c1ef12e432fbf7791768376b0f23bea005
Dubons <rosaliodubon@gmail.com>
2024-12-01 19:33:28 -0600

    se agregan cambios a la seccion de contacto

--- script.js
+++ script.js
@@ -15,30 +15,6 @@ function seleccionar(){
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
-//Funcion que aplica las animaciones de las habilidades
-/*function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("Angular");
-        habilidades[3].classList.add("springboot");
-        habilidades[4].classList.add("postgressq");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
-    }
-}
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} */
 
 //metodo para descargar
 function descargarPDF() {
@@ -56,4 +32,6 @@ function descargarPDF() {
     // Elimina el enlace del documento HTML
     document.body.removeChild(link);
 }
+
+
 
